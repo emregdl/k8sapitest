5256ff6b65b373e16327a67e5ca8aad30e11158c
emregdl <78479982+emregdl@users.noreply.github.com>
2026-06-16 16:27:00 +0300

    Update k8s_client.py

--- app/k8s_client.py
+++ app/k8s_client.py
@@ -1,14 +1,20 @@
 from kubernetes import client, config
+from kubernetes.config.config_exception import ConfigException
 
-KUBECONFIG_PATH = r"C:\Users\omer.gudul\Desktop\k8stest.kubeconfig"
-config.load_kube_config(config_file=KUBECONFIG_PATH)
-
-core_v1 = client.CoreV1Api()
-custom_api = client.CustomObjectsApi()
 
 def get_core_v1_client():
-    return core_v1
+    try:
+        config.load_incluster_config()
+    except ConfigException:
+        config.load_kube_config()
+
+    return client.CoreV1Api()
 
 
-def get_custom_objects_client():
-    return custom_api
+def get_apps_v1_client():
+    try:
+        config.load_incluster_config()
+    except ConfigException:
+        config.load_kube_config()
+
+    return client.AppsV1Api()
